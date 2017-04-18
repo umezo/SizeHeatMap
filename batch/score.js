d8be188b86f14700e138e5ab0b64f07fd266a99d
umezo <umezo@tsuyabu.in>
2017-04-19 02:10:29 +0900

    batchファイルをリファクタに対応

--- batch/score.js
+++ batch/score.js
@@ -1,99 +1,49 @@
 /* eslint-disable no-console */
-var fs = require('fs');
-var readline = require('readline');
-
-getResolutionData('part-r-00000', function(err, result){
-  const data = calculateScore(result.data, result.maxW, result.maxH);
-  console.error('output score');
-  console.log(JSON.stringify({
-    sample:result.data.length,
-    width:result.maxW,
-    height:result.maxH,
-    score:data
-  }));
-});
+const commander = require('commander');
+const { readResolutionDataFile } = require( '../lib/file-read-helper');
+const { calculateScore } = require( '../lib/calculate-score');
 
 const DATA_LIMIT = 100000 * 2;
 const W_LIMIT = 2000;
 const H_LIMIT = 1500;
 
-function parseLineToWHArray(line) {
-  const param = line.split('\t');
-  const w = Math.min(parseInt(param[3], 10), W_LIMIT);
-  const h = Math.min(parseInt(param[4], 10), H_LIMIT);
 
-  return [w, h];
-}
+commander
+  .option('--file [filepath]', 'path to resolution data')
+  .parse(process.argv);
 
+  console.log(commander);
+
+readResolutionDataFile(commander.file, parseLineToWHArray, DATA_LIMIT)
+  .then(function(result){
+    const data = calculateScore(result.data, result.maxW, result.maxH);
+    console.error('output score');
+    console.log(JSON.stringify({
+      sample:result.data.length,
+      width:result.maxW,
+      height:result.maxH,
+      score:data
+    }));
+  })
+  .catch(function (error){
+    throw error;
+  });
 
 /**
- * @param {string} filepath 読み込むファイルのパス
- * @param {function} 読み込んだファイルの一行を受け取って、[number, number] の配列に変換する関数
- *
- * @return {Promise} successすると {data:{Array<Array<number>>}, maxW:{number} maxH:{number}}
+ * 1980x1080\t2000 みたいな行をパースする
  */
-function getResolutionData(filepath, parseLineToWHArray){
-  //readlineってエラーイベント無いの？マジ？
-  return new Promise( (resolve) => {
-    const rs = fs.ReadStream(filepath);
-    const readlineInterface = readline.createInterface({'input': rs, 'output': {}});
+function parseLineToWHArray(line) {
+  console.log(line);
+  const param = line.split('\t');
+  const resolution = param[0].split('x');
 
-    let maxW = 0;
-    let maxH = 0;
-    let data = [];
-
-    readlineInterface.on('line', function (line) {
-      if(line.trim() === ''){
-        return;
-      }
-
-      const [w, h] = parseLineToWHArray(line);
-
-      maxW = Math.max(maxW, w);
-      maxH = Math.max(maxH, h);
-      data.push([w, h]);
-      if (data.length >= DATA_LIMIT) {
-        readlineInterface.close();
-      }
-    });
-
-    readlineInterface.on('close', function(){
-      resolve({
-        data:data,
-        maxW:maxW,
-        maxH:maxH
-      });
-    });
-
-    readlineInterface.resume();
-  });
-}
-
-function calculateScore(data, maxW, maxH){
-
-  let score = new Array(maxW*maxH);
-  for (var i = 0 ; i < score.length ; i++) {
-    score[i] = 0;
+  if (resolution.length < 2) {
+    return null;
   }
 
-  data.forEach(function(rect, rowIndex){
-    const w = rect[0];
-    const h = rect[1];
+  const w = Math.min(parseInt(resolution[0], 10), W_LIMIT);
+  const h = Math.min(parseInt(resolution[1], 10), H_LIMIT);
+  const count = parseInt(param[1], 10);
 
-    let i, j, index;
-
-    for (j = 0 ; j < h ; j++) {
-      for (i = 0 ; i < w ; i++) {
-        index = j * maxW + i;
-        score[index]++;
-      }
-    }
-
-    if (rowIndex % 1000 === 0) {
-      console.error(rowIndex+'/'+data.length);
-    }
-  });
-
-
-  return score;
+  return [w, h, count];
 }
