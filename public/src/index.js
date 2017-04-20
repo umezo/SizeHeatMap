a6f5deb4dcdbdb9e48016d36accb51e56d331afd
umezo <umezo@tsuyabu.in>
2017-04-21 08:54:14 +0900

    resetして作業再開

--- public/src/index.js
+++ public/src/index.js
@@ -1,12 +1,12 @@
+/* eslint-disable no-console */
 const $ = require('jquery');
-const _ = require('lodash');
+const score = require('../score.json');
 
 var canvas = $('#practiceCanvas')[0];
 
 console.time('createHeatMap');
 
-createGuide();
-loadScore();
+loadScore(score);
 
 function createGuide(){
   var container = $('#container');
@@ -18,29 +18,24 @@ function createGuide(){
     });
 
     container.append(div);
-
   }
 }
 
-function loadScore(){
-  $.getJSON('score.json', {}, function(data){
-    var imageData = createHeatMap(
-        canvas,
-        data.width,
-        data.height,
-        data.score,
-        data.sample
-      );
+function loadScore(data) {
+  var imageData = createHeatMap(
+      canvas,
+      data.width,
+      data.height,
+      data.score,
+      data.sample
+    );
 
-    console.log('draw canvas');
-    canvas.width  = imageData.width;
-    canvas.height = imageData.height;
-    canvas.getContext('2d').putImageData(imageData, 0, 0);
+  console.log('draw canvas');
+  canvas.width  = imageData.width;
+  canvas.height = imageData.height;
+  canvas.getContext('2d').putImageData(imageData, 0, 0);
 
-    $(canvas).css({width:canvas.width, height:canvas.height});
-  });
-
-
+  $(canvas).css({width:canvas.width, height:canvas.height});
 }
 
 function createHeatMap(canvas, w, h, score, maxScore) {
@@ -49,10 +44,10 @@ function createHeatMap(canvas, w, h, score, maxScore) {
   var ctx = canvas.getContext('2d');
   var img = ctx.createImageData(w, h);
   var data = img.data;
-  _.forEach(score, function(s, index){
+  score.forEach(function(s, index){
     var r = index * 4 + 0;
-    var g = index * 4 + 1;
-    var b = index * 4 + 2;
+    //var g = index * 4 + 1;
+    //var b = index * 4 + 2;
     var a = index * 4 + 3;
 
     var rate = parseInt(s/maxScore * 10, 10) / 10;
@@ -62,35 +57,4 @@ function createHeatMap(canvas, w, h, score, maxScore) {
   });
 
   return img;
-}
-
-function calculateScore(data, maxW, maxH, done){
-  maxW = Math.min(2000, maxW);
-  maxH = Math.min(2000, maxH);
-  console.log('max = ('+maxW+','+maxH+')');
-
-
-
-  var handler = {
-    progress:function(e){
-      console.log(e.data.progress);
-    },
-    complete:function(e){
-      done({score:e.data.score, maxScore:data.length});
-    }
-  };
-
-  console.log('Worker kick');
-  var worker = new Worker('worker.js');
-  worker.addEventListener('message', function(e){
-    console.log(e);
-    var data = e.data;
-    handler[data.cmd](e);
-  });
-  worker.postMessage({
-    cmd:'score',
-    data:data,
-    maxW:maxW,
-    maxH:maxH
-  });
 }
